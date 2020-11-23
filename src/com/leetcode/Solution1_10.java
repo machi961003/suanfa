e4249f80f60687d289d1e59a2fd686c322afa797
machi <machi961003@gmail.com>
2020-11-23 21:30:46 +0800

    helloworldpp

--- src/com/leetcode/Solution1_10.java
+++ src/com/leetcode/Solution1_10.java
@@ -7,6 +7,6 @@ package com.leetcode;
 public class Solution1_10 {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hellppo");
     }
 }
